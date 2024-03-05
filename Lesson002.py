a1a8f93c11a50f7f50ff5e6ec1a32e80f117e2b0
jorj <sandybaev@mail.ru>
2024-03-05 10:51:43 +0500

    диалог запроса пароля

--- Lesson002.py
+++ Lesson002.py
@@ -2,12 +2,11 @@ import turtle
 num_circles = 36
 radius = 100
 angle = 10
-animation_speed = 0
+animation_speed = 13
 
 turtle.speed(animation_speed)
 
 for x in range(num_circles):
     turtle.circle(radius)
     turtle.left(angle)
-
 turtle.done()
